2bb6816f8553089a5316871fd166fd202defa360
mayalexandera <maya.blevins@gmail.com>
2020-07-05 23:41:24 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  validates_presence_of :name, allow_blank: false
+  validates_uniqueness_of :email, presence: true
+  validates_length_of :phone_number, { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates_presence_of :title, allow_blank: false 
+  validates_length_of :content, { minimum: 100}
+  validates_inclusion_of :category, in: %w[Fiction Non-Fiction]
+  
 end
